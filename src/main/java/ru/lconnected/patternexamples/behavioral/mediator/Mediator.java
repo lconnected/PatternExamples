58b37b04d72c804073326740f3d43dc393e3b317
Lev Khruschev <lkhruschev@alvioneurope.ru>
2016-11-11 12:55:56 +0300

    Pattern Mediator description added

--- src/main/java/ru/lconnected/patternexamples/behavioral/mediator/Mediator.java
+++ src/main/java/ru/lconnected/patternexamples/behavioral/mediator/Mediator.java
@@ -1,6 +1,8 @@
 package ru.lconnected.patternexamples.behavioral.mediator;
 
 /**
+ * Является посредником для взаимодействия нескольких объектов
+ * Уменьшает явную связь между ними
  * Created by lkhruschev on 11.11.2016.
  * lkhruschev@alvioneurope.ru
  * Skype: levasfx
